35e4d8831f551d454fb3e6a161aa87c8899c4d0a
ksw13 <ksw48483@gmail.com>
2023-12-31 19:55:28 +0900

    feat: #3-repository 생성

---
+++ src/main/java/com/example/demo/repository/ArticleCommentRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+
+}
