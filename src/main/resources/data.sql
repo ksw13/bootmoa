6feacff42b0a9e8d85414c4bbdba96e4f8a51641
ksw13 <ksw48483@gmail.com>
2024-01-02 07:10:21 +0900

    feat: #10-dto 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,92 +1,31 @@
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Vandalia Municipal Airport', 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
+insert into user_account (user_id, user_password, created_at, created_by, modified_at, modified_by) values
+    ('swkang', '{noop}asdf1234', now(), 'swkang', now(), 'swkang')
+;
+insert into article (id, user_id, title, content, category, created_at, created_by, modified_at, modified_by) values (1, 'swkang', 'Heihe Airport', 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.', 'Rebar & Wire Mesh Install', '2023-05-27 23:54:36', 'Anna-diana', '2023-08-20 16:47:12', 'Christen');
+insert into article (id, user_id, title, content, category, created_at, created_by, modified_at, modified_by) values (2, 'swkang', 'San José De Chiquitos Airport', 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.', 'Fire Sprinkler System', '2023-04-15 06:36:02', 'Baron', '2023-07-08 01:52:49', 'Arnold');
+insert into article (id, user_id, title, content, category, created_at, created_by, modified_at, modified_by) values (3, 'swkang', 'Malden Regional Airport', 'Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.
 
-Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 'Drilled Shafts', 'Taite', '2023-04-11 13:21:48', 'Madison', '2023-05-02 06:37:57');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Rio Mayo Airport', 'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.', 'Fire Protection', 'Albina', '2023-01-28 08:33:04', 'Avigdor', '2023-03-14 12:50:28');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Guanambi Airport', 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
+Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.
 
-Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
+Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', 'Temp Fencing, Decorative Fencing and Gates', '2023-08-28 16:37:50', 'Lin', '2023-07-23 16:48:12', 'Sallie');
+insert into article (id, user_id, title, content, category, created_at, created_by, modified_at, modified_by) values (4, 'swkang', 'Huallaga Airport', 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.', 'Elevator', '2023-10-11 00:57:51', 'Rosemaria', '2023-04-26 21:01:51', 'Byran');
+insert into article (id, user_id, title, content, category, created_at, created_by, modified_at, modified_by) values (5, 'swkang', 'Beslan Airport', 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.
 
-Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 'Structural & Misc Steel Erection', 'Gerick', '2023-12-03 16:10:32', 'Neddie', '2023-07-27 15:34:10');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Yorke Island Airport', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
+Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.
 
-Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', 'Ornamental Railings', 'Ina', '2023-07-19 19:11:53', 'Marcelia', '2023-07-11 22:00:17');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Tichitt Airport', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.
+Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', 'Sitework & Site Utilities', '2023-08-06 15:48:57', 'Thomasa', '2023-01-30 12:03:00', 'Willow');
+insert into article (id, user_id, title, content, category, created_at, created_by, modified_at, modified_by) values (6, 'swkang', 'Martindale Army Heliport', 'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.
 
-Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.
+Sed ante. Vivamus tortor. Duis mattis egestas metus.
 
-Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.', 'Electrical', 'Ann', '2023-09-11 21:00:29', 'Corenda', '2023-09-26 23:02:42');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Leonardo da Vinci–Fiumicino Airport', 'Phasellus in felis. Donec semper sapien a libero. Nam dui.
+Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.', 'RF Shielding', '2023-04-05 23:30:41', 'Vilhelmina', '2023-09-01 06:34:01', 'Britta');
+insert into article (id, user_id, title, content, category, created_at, created_by, modified_at, modified_by) values (7, 'swkang', 'Itabuna Airport', 'Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.
 
-Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.', 'Electrical and Fire Alarm', 'Anderea', '2023-09-20 17:33:40', 'Lory', '2023-05-18 19:21:45');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Pune Airport', 'In congue. Etiam justo. Etiam pretium iaculis justo.
+In congue. Etiam justo. Etiam pretium iaculis justo.', 'Waterproofing & Caulking', '2023-02-10 20:17:14', 'Henrik', '2023-03-02 09:02:00', 'Kacie');
+insert into article (id, user_id, title, content, category, created_at, created_by, modified_at, modified_by) values (8, 'swkang', 'Edinburgh Airport', 'Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.
 
-In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.
+Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.', 'Rebar & Wire Mesh Install', '2023-10-29 09:49:27', 'Anselm', '2023-04-09 22:18:52', 'Galina');
+insert into article (id, user_id, title, content, category, created_at, created_by, modified_at, modified_by) values (9, 'swkang', 'Chan Gurney Municipal Airport', 'Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
 
-Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', 'Structural & Misc Steel Erection', 'Randall', '2023-01-12 08:56:11', 'Jeffie', '2023-05-23 16:22:35');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Zhytomyr Airport', 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
-
-Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 'Sitework & Site Utilities', 'Reagan', '2023-07-22 13:22:51', 'Ches', '2023-03-30 01:15:50');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Biaugunu Airport', 'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.', 'Roofing (Metal)', 'Inga', '2023-10-26 23:41:47', 'Wilmette', '2023-12-24 09:18:14');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Grayling Airport', 'Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.
-
-Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.', 'Soft Flooring and Base', 'Marga', '2023-02-04 21:05:16', 'Vladimir', '2023-03-24 12:13:52');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Alberto Carnevalli Airport', 'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.', 'Structural and Misc Steel (Fabrication)', 'Justina', '2023-08-26 02:11:31', 'Lilli', '2023-07-29 01:54:18');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Valladolid Airport', 'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.', 'Termite Control', 'Noellyn', '2023-08-28 09:33:27', 'Grete', '2023-07-08 03:25:59');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Baleela Airport', 'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.
-
-Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.
-
-Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.', 'Electrical', 'Burnaby', '2023-11-06 02:16:15', 'Flss', '2023-09-04 04:26:38');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Laval-Entrammes Airport', 'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.', 'Doors, Frames & Hardware', 'Orbadiah', '2023-03-10 22:10:55', 'Linea', '2023-02-12 20:10:28');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Zona da Mata Regional Airport', 'Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
-
-In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.
-
-Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.', 'Sitework & Site Utilities', 'Raimund', '2023-05-16 18:50:22', 'Valentino', '2023-11-04 19:58:45');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Los Alamos Airport', 'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.
-
-Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.
-
-Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.', 'Termite Control', 'Jared', '2023-09-16 09:57:13', 'Hedvig', '2023-04-04 11:44:01');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Palapye Airport', 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.
-
-Phasellus in felis. Donec semper sapien a libero. Nam dui.', 'Roofing (Asphalt)', 'Grantham', '2023-11-13 05:00:15', 'Currie', '2023-07-09 01:33:50');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Ivalo Airport', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', 'Rebar & Wire Mesh Install', 'Pat', '2023-02-04 15:24:39', 'Dewey', '2023-04-08 16:58:53');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Bearskin Lake Airport', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-
-Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.
-
-Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.', 'Roofing (Asphalt)', 'Lina', '2023-08-28 16:47:01', 'Jaquenette', '2023-06-30 22:09:36');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Vichy-Charmeil Airport', 'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.
-
-Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.', 'Asphalt Paving', 'Brandea', '2023-03-14 17:42:32', 'Tildie', '2023-02-09 20:07:47');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Deer Lake Airport', 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 'Retaining Wall and Brick Pavers', 'Kai', '2023-01-02 05:35:15', 'Harlen', '2023-09-29 14:33:08');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Adado Airport', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
-
-Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
-
-In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.', 'Painting & Vinyl Wall Covering', 'Rosalinda', '2023-09-04 10:40:54', 'Carmelina', '2023-11-16 08:46:14');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Tobago-Crown Point Airport', 'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.
-
-Sed ante. Vivamus tortor. Duis mattis egestas metus.', 'Waterproofing & Caulking', 'Minny', '2023-09-19 07:54:01', 'Hy', '2023-01-24 01:46:59');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Massawa International Airport', 'Fusce consequat. Nulla nisl. Nunc nisl.
-
-Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.', 'Casework', 'Hanny', '2023-11-16 18:31:12', 'Margret', '2023-02-25 05:00:37');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Bahawalpur Airport', 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.
-
-Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.', 'Framing (Wood)', 'Clemens', '2023-12-17 00:43:51', 'Magdaia', '2023-10-04 09:44:12');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Shubuling Airport', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
-
-Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
-
-In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.', 'Retaining Wall and Brick Pavers', 'Tamas', '2023-09-21 15:47:42', 'Benetta', '2023-07-10 08:25:14');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Pukatawagan Airport', 'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.', 'Glass & Glazing', 'Freddi', '2023-05-07 19:29:36', 'Rosanna', '2023-03-27 00:31:21');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Tazadit Airport', 'Fusce consequat. Nulla nisl. Nunc nisl.
-
-Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.', 'Waterproofing & Caulking', 'Christoffer', '2023-01-08 05:23:26', 'Georgine', '2023-11-03 02:03:06');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Bert Mooney Airport', 'Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.
-
-Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.', 'Curb & Gutter', 'Chandal', '2023-04-12 00:23:16', 'Ginny', '2023-03-09 09:08:03');
-insert into article (title, content, category, created_by, created_at, modified_by, modified_at) values ('Anfa Airport', 'Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.
-
-Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.', 'Site Furnishings', 'Eleen', '2023-01-20 18:33:19', 'Kippie', '2023-09-24 16:12:42');
+In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.', 'Glass & Glazing', '2023-09-01 03:47:45', 'Johnathon', '2023-12-23 17:04:16', 'Mill');
+insert into article (id, user_id, title, content, category, created_at, created_by, modified_at, modified_by) values (10, 'swkang', 'Rajouri Airport', 'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.', 'Drilled Shafts', '2023-05-14 22:51:39', 'Tiffany', '2023-09-02 23:29:41', 'Christophe');
