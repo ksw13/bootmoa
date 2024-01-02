7d87a6fd4a3a3a52c82e92c8162a2a82403a10d1
ksw13 <ksw48483@gmail.com>
2024-01-02 10:59:19 +0900

    feat: #9-게시판 뷰 구현

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -1,25 +1,44 @@
 package com.example.demo.controller;
 
-import java.util.List;
+import com.example.demo.domain.type.SearchType;
+import com.example.demo.dto.ArticleWithCommentDto;
+import com.example.demo.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+    private final ArticleService articleService;
+
+    // 게시판 리스트 출력
     @GetMapping
-    public String articles(ModelMap map){
-        map.addAttribute("articles", List.of());
+    public String articles(
+            @RequestParam(required = false) SearchType searchType,
+            @RequestParam(required = false) String keyword,
+            // pageable에 대한 옵션
+            @PageableDefault(size = 10, sort = "createdAt", direction = Direction.DESC) Pageable pageable,
+            ModelMap map
+    ){
+        map.addAttribute("articles", articleService.searchArticles(searchType, keyword, pageable));
         return "articles/index";
     }
 
     @GetMapping("/{articleId}")
     public String article(@PathVariable Long articleId, ModelMap map){
-        map.addAttribute("article", null);
-        map.addAttribute("articleComments", List.of());
+        ArticleWithCommentDto article = articleService.getArticle(articleId);
+        map.addAttribute("article", article);
+        map.addAttribute("articleComments", article.getArticleCommentDtos());
 
         return "articles/detail";
     }
